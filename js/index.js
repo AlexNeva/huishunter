cfed9fe17c4c0ef724a8669be4c29f527aec86a5
AlexNeva <68752504+AlexNeva@users.noreply.github.com>
2021-09-03 13:23:37 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -111,3 +111,33 @@ document.addEventListener('DOMContentLoaded', () => {
   // Scroll to Anchors init ==================
 
 });
+
+$(function () {
+  function countup(className) { //className - имя класса, в котором есть число
+    var countBlockTop = $(className).offset().top; //смещение блока с числом относительно верхнего края	
+    var windowHeight = window.innerHeight;//высота окна браузера
+    var show = true;// отвечает, что если один раз счетчик сработает, больше не срабатывал
+
+    $(window).scroll(function () {
+      if (show && (countBlockTop < $(window).scrollTop() + windowHeight)) {
+        show = false; //если мы видим число, то больше его не надо показывать
+
+        $(className).spincrement({ //вызов плагина с параметрами 
+          from: 1,               //начинать с 1
+          duration: 4000,        //задержка счетчика
+        });
+      }
+    })
+  }
+
+
+
+
+
+
+  countup("#count1");
+  countup("#count2");
+  countup("#count3");
+  countup("#count4");
+
+});
